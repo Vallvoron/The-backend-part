57ff098b115bd03bbd22c50b30d0c230559a4587
4rsess <misha.d.nosov@gmail.com>
2025-02-24 14:10:37 +0700

    First build

---
+++ src/main/java/com/example/SkippingLessonsJavaProject/SkippingLessonsJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkippingLessonsJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkippingLessonsJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkippingLessonsJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SkippingLessonsJavaProject/SkippingLessonsJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkippingLessonsJavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkippingLessonsJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
