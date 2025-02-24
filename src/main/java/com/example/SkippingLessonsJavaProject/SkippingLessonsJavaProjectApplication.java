12d47ce50e5a545c0d725c7e5ecf2400aa4c81f0
4rsess <misha.d.nosov@gmail.com>
2025-02-24 14:14:31 +0700

    Second commit

--- src/main/java/com/example/SkippingLessonsJavaProject/SkippingLessonsJavaProjectApplication.java
+++ src/main/java/com/example/SkippingLessonsJavaProject/SkippingLessonsJavaProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkippingLessonsJavaProjectApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SkippingLessonsJavaProjectApplication.class, args);
 	}
